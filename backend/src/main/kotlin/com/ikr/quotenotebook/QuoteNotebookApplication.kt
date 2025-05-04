1fce9d515da5fe21a197dc6a24d582e6bce745f5
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2025-05-04 22:46:19 +0200

    Initial commit

---
+++ backend/src/main/kotlin/com/ikr/quotenotebook/QuoteNotebookApplication.kt
@@ -0,0 +1,11 @@
+package com.ikr.quotenotebook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuoteNotebookApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuoteNotebookApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/ikr/quotenotebook/QuoteNotebookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ikr.quotenotebook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuoteNotebookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
